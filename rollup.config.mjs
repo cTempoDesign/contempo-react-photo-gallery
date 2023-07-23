ce95e2b0a74af739cf60b778126d52b58c4e7945
Karl Laptop <contempowebdesign@gmail.com>
2023-07-22 23:03:47 -0500

    beta 6: include css files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -29,7 +29,7 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      postcss(),
+      postcss({ extract: true }),
       scss(),
       terser()
     ],
