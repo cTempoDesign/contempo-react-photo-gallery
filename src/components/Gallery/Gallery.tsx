c6068347b94481834c3c7e44736f60f1d532a036
Karl Laptop <contempowebdesign@gmail.com>
2023-08-06 16:50:25 -0500

    WIP: adding image overlay

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FC, useState } from "react";
 import "./Gallery.css";
 
 export type PhotoConfig = {
@@ -10,24 +10,57 @@ export type GalleryProps = {
   images: PhotoConfig[];
   spacing?: string;
   lazy?: boolean;
+  enableScroller?: boolean | true;
 };
 
-const Gallery = ({ images, lazy }: GalleryProps) => {
+export interface OverlayProps {
+  onClose: () => void;
+}
+
+const Overlay: FC<OverlayProps> = ({ onClose }) => {
   return (
-    <div className="wrapper">
-      <div className="grid-container">
-        {images.map((photo: PhotoConfig, index: number) => (
-          <img
-            style={{ width: "100%" }}
-            key={index}
-            src={photo.path}
-            alt={photo.alt}
-            loading={lazy ? "lazy" : "eager"}
-          />
-        ))}
+    <div id="imageOverlay" className="overlay">
+      <span onClick={onClose} className="close cursor">
+        CLOSE
+      </span>
+      <div className="modal-contempo">
+        <div className="mySlides responsive">
+          <div className="numbertext"></div>
+          <img className="responsive"></img>
+        </div>
+        <a className="image-button prev">
+          <i className="material-icons caro-icon md-58">chevron_left</i>
+        </a>
+        <a className="image-button next">
+          <i className="material-icons caro-icon md-58">chevron_right</i>
+        </a>
       </div>
     </div>
   );
 };
 
-export default Gallery;
+const ContempoGallery = ({ images, lazy }: GalleryProps) => {
+  const [showOverlay, setShowOverlay] = useState(false);
+
+  return (
+    <div className="wrapper">
+      <div className="grid-container">
+        {images.map((photo: PhotoConfig, index: number) => (
+          <span onClick={() => setShowOverlay(true)} key={index}>
+            <img
+              style={{ width: "100%" }}
+              src={photo.path}
+              alt={photo.alt}
+              loading={lazy ? "lazy" : "eager"}
+            />
+          </span>
+        ))}
+        {showOverlay && (
+          <Overlay onClose={() => setShowOverlay(false)}></Overlay>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ContempoGallery;
